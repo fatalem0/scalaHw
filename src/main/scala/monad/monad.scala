a7cd9ed3e702d48183e6c9ec7f160a1af796bf8a
fatalem0 <madchaterrer@gmail.com>
2023-01-19 17:55:03 +0300

    add hw3

--- src/main/scala/monad/monad.scala
+++ src/main/scala/monad/monad.scala
@@ -9,21 +9,17 @@ package object monad {
 
     def get: A
 
-    def pure[R](x: R): Wrap[R] = ???
+    def pure[R](x: R): Wrap[R] = NonEmptyWrap(x)
 
-    def flatMap[R](f: A => Wrap[R]): Wrap[R] = {
-      ???
+    def flatMap[R](f: A => Wrap[R]): Wrap[R] = this match {
+      case NonEmptyWrap(x) => f(x)
+      case _ => EmptyWrap
     }
 
     // HINT: map можно реализовать через pure и flatMap
-    def map[R](f: A => R): Wrap[R] = {
-      ???
-    }
+    def map[R](f: A => R): Wrap[R] = this.flatMap(x => pure(f(x)))
 
-    def withFilter(f: A => Boolean): Wrap[A] = {
-      ???
-    }
-
+    def withFilter(f: A => Boolean): Wrap[A] = this.flatMap(x => if (f(x)) pure(x) else EmptyWrap)
   }
 
   object Wrap {
